1db10f6fa1e2411c07ef03e3ae842312ff553394
bmenezes05 <bmenezes05@gmail.com>
2024-06-24 22:09:22 -0300

    Added main menu and initial logic

---
+++ MysticExpeditions.Client/Models/GameEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace MysticExpeditions.Client.Models
+{
+    public class GameEvent
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Dictionary<string, Action> Choices { get; set; }
+    }
+}
--- MysticExpeditions.Server/Program.cs
+++ MysticExpeditions.Server/Program.cs
@@ -1,5 +1,3 @@
-using MysticExpeditions.Server.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,8 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
